015e96ef893317011acf6742123fe21d7bb382dc
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-11-28 15:50:45 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
 for char in range(ord('a'), ord('z') + 1):
-    if chr(char) != 'q' or chr(char) != 'e':
+    if chr(char) not in ['q', 'e']:
         print("{}".format(chr(char)), end="")
-
