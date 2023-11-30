702cbb309d8bfb53326c6f8d23e0873776d4d403
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-11-29 16:41:21 -0800

    Update: 2023-11-29 16:41:21

--- git-update.sh
+++ git-update.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-# Check if there are any changes to commit
-if git diff --exit-code --quiet; then
-    echo "No changes to commit."
-else
-    # Add all changes
-    git add .
-
-    # Commit changes with a default commit message
-    git commit -m "Update: $(date '+%Y-%m-%d %H:%M:%S')"
-
-    # Push changes to the origin repository
-    git push
-fi
-
+git add .
+git commit -m "Update: $(date '+%Y-%m-%d %H:%M:%S')"
+git push
