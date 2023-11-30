0793b9d9632eb8e0227a03a082f04de2a31516b8
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-11-29 18:45:21 -0800

    Update: 2023-11-29 18:45:21

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 for i in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(i) if i % 2 == 1 else chr(i - 32)), end="")
+    print("{}".format(chr(i) if (ord('z') - i) % 4 < 2 else chr(i - 32)), end="")
