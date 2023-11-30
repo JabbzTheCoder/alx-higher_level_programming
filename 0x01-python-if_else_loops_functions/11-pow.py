dc8e58f94b4946f97e7c2e7351594c53d18aca3a
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-11-29 18:14:29 -0800

    Update: 2023-11-29 18:14:29

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    return a ^ b
+    result = 1
+
+    # Handle the case when b is negative
+    if b < 0:
+        a = 1 / a
+        b = -b
+
+    for _ in range(b):
+        result *= a
+
+    return result
