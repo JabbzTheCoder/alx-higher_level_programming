e7e00c2f6dee34e90d86302478b525caed6c6d70
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-12-06 11:50:21 -0800

    Update: 2023-12-06 11:50:21

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-    # Check if the matrix is empty
     if not matrix:
         return []
 
@@ -10,7 +9,6 @@ def square_matrix_simple(matrix=[]):
     num_cols = len(matrix[0])
     new_matrix = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
 
-    # Iterate through the elements of the input matrix and square them
     for i in range(num_rows):
         for j in range(num_cols):
             new_matrix[i][j] = matrix[i][j] ** 2
