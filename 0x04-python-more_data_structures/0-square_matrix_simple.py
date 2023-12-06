1fbb28ff5c9dd964549f58bb536f4314dabb0b40
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-12-06 11:55:35 -0800

    Update: 2023-12-06 11:55:35

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        return []
+    new_matrix = matrix.copy()
 
-    # Create a new matrix with the same dimensions as the input matrix
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-    new_matrix = [[0 for _ in range(num_cols)] for _ in range(num_rows)]
+    for i in range(len(matrix)):
+        new_matrix[i] = list(map(lambda x: x**2, matrix[i]))
 
-    for i in range(num_rows):
-        for j in range(num_cols):
-            new_matrix[i][j] = matrix[i][j] ** 2
-
-    return new_matrix
+    return (new_matrix)
