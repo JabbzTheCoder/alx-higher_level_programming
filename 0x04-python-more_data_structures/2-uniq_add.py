2f74bff00e844c6f6415570df855176e2ecd9e6f
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-12-06 12:41:52 -0800

    Update: 2023-12-06 12:41:52

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    num_to_add = []
+    results = 0
+
+    for i in range(len(my_list)):
+        if (my_list[i] not in num_to_add):
+            num_to_add.append(my_list[i])
+
+    
+    results = map(lambda x: rusults + x, my_list)
