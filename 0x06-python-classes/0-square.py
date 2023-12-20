bda68af5e32fc489d5705df7ddbee84809bf5972
JabbzTheCoder <jabbzthecoder@gmail.com>
2023-12-20 01:28:38 -0800

    Update: 2023-12-20 01:28:38

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    A simple class representing a square.
+
+    Attributes:
+    - No specific attributes defined yet.
+
+    Methods:
+    - No specific methods defined yet.
+    """
+    pass
